f41641fbf83ed4ca3c22f9fdb2b0f43d9ec1529b
Marium-Akter-Dev <mariumshorna1329@gmail.com>
2024-10-21 18:13:53 +0600

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+  return (
+    <>
+    <h1>hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto reiciendis eum 
+        laudantium, ullam rem laboriosam sunt repellendus eveniet ea repudiandae, nesciunt 
+        amet rerum obcaecati hic dolores molestias praesentium? Reiciendis, magni dolor! 
+        Possimus reprehenderit omnis minus doloribus modi doloremque. Possimus sapiente 
+        tenetur perspiciatis ratione consequuntur quidem libero voluptates dolor facere 
+        perferendis?
+    </p>
+    </>
+  )
+}
+
+export default Navbar
